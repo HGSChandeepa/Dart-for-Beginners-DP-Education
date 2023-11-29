c1569de73eb78eb1698ad50a01a0448b0561814c
samin <84340577+HGSChandeepa@users.noreply.github.com>
2023-11-29 19:39:31 +0530

    switch-case

--- control_flow_if_else(basics).dart
+++ control_flow_if_else(basics).dart
@@ -108,4 +108,25 @@ void main() {
   } else {
     print("water level is not provided"); 
   }
+
+  //marks calculation
+  //define the condition
+  //A - 75 or more 
+  //B - 65 or more
+  //C - 55 or more
+  //S - 45 or more
+  //W - less than 45
+  
+  int marks = 80;
+  if (marks >= 75) {
+    print("A");
+  } else if (marks >= 65) {
+    print("B");
+  } else if (marks >= 55) {
+    print("C");
+  } else if (marks >= 45) {
+    print("S");
+  } else {
+    print("W");
+  }
 }
--- types.dart
+++
@@ -1,9 +0,0 @@
-/*
-
-Dart is a statically-typed programming language, which means that the type of a variable is known at compile time. This allows for early error checking and can lead to more efficient code execution. Dart also supports type inference, which means that you don't always have to explicitly declare the type of a variable
-
-*/
-
-void main(){
-  
-}
