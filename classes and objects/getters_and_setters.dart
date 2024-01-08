05eaa13b1c258d35e1320d2de1d0e426d8e3448b
samin <84340577+HGSChandeepa@users.noreply.github.com>
2024-01-08 21:05:05 +0530

    getters and setters

--- classes and objects/getters_and_setters.dart
+++ classes and objects/getters_and_setters.dart
@@ -1,3 +1,115 @@
-void main(){
-  
+// Introduction to the Dart getter & setter
+
+class Circle {
+  double _radius = 0;
+
+  //getter
+  double get radius {
+    print("getter called");
+    return _radius;
+  }
+
+  //setter
+  set radius(double value) {
+    if (value > 0) {
+      _radius = value;
+    } else {
+      _radius = 0;
+    }
+
+    print("setter called and radius is $_radius");
+  }
+}
+
+//---------------------------------------
+
+class Person {
+  String? _firstName;
+  String? _lastName;
+  int? _age;
+
+  // Constructor with named parameters
+  Person(
+      {required String firstName, required String lastName, required int age}) {
+    _firstName = this.firstName;
+    _lastName = this.lastName;
+    _age = this.age;
+  }
+
+  // Getter and setter for the first name property
+  String get firstName {
+    if (_firstName != null) {
+      return _firstName!;
+    } else {
+      return "";
+    }
+  }
+
+  set firstName(String newName) {
+    if (newName.isNotEmpty) {
+      _firstName = newName;
+    } else {
+      print("First name cannot be empty.");
+    }
+  }
+
+  // Getter and setter for the last name property
+  String get lastName {
+    if (_lastName != null) {
+      return _lastName!;
+    } else {
+      return "";
+    }
+  }
+
+  set lastName(String newName) {
+    if (newName.isNotEmpty) {
+      _lastName = newName;
+    } else {
+      print("Last name cannot be empty.");
+    }
+  }
+
+  // Getter and setter for the age property
+  int get age {
+    if (_age != null) {
+      return _age!;
+    } else {
+      return 0;
+    }
+  }
+
+  set age(int newAge) {
+    if (newAge >= 0 && newAge <= 150) {
+      _age = newAge;
+    } else {
+      print("Age must be between 0 and 150.");
+    }
+  }
+
+  // Getter for the full name property
+  String get fullName => '$_firstName $_lastName';
+}
+
+void main() {
+  Circle circle = Circle();
+  //calling the setter and setting the value of radius as 5
+  circle.radius = 5;
+  //calling the getter
+  print(circle.radius);
+  //calling the setter and setting the value of radius as -5
+  circle.radius = -5;
+  //calling the getter
+  print(circle.radius);
+
+  //---------------------
+  Person person = Person(firstName: "John", lastName: "Doe", age: 30);
+  print(person.fullName); // John Doe
+  person.firstName = "Jane";
+  person.lastName = "Smith";
+  person.age = 25;
+  print(person.fullName); // Jane Smith
+  print(person.age); // 25
+  person.age = 200; // Age must be between 0 and 150.
+  print(person.age); // 25
 }
