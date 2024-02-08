5c01098f8881f700fd07f1d3f87b868a5f88d5e0
samin <84340577+HGSChandeepa@users.noreply.github.com>
2024-02-08 12:41:37 +0530

    changes

--- asynchronous/futures.dart
+++ asynchronous/futures.dart
@@ -1,14 +1,14 @@
 //?whith out async and await keywords
 //here we are using then method to get the value of future but the future is not yet completed so the then method is not executed and the value is not printed on the console until the future is completed.
 
-void main() {
-  print(1);
+// void main() {
+//   print(1);
 
-  var future = Future<int>.delayed(Duration(seconds: 0), () => 2);
-  future.then((value) => print(value));
+//   var future = Future<int>.delayed(Duration(seconds: 0), () => 2);
+//   future.then((value) => print(value));
 
-  print(3);
-}
+//   print(3);
+// }
 
 // ?with async and await keywords
 //here we are using await keyword to get the value of future and the future is not yet completed so the await keyword will wait for the future to complete and then the value is printed on the console.
@@ -27,7 +27,7 @@ void main() {
 
 // If an asynchronous operation results in an error, you can use the try-catch block to handle it. For example
 
-// Future<void> main() async {
+// void main() async {
 //   print(1);
 //   try {
 //     var value = await Future<int>.delayed(Duration(seconds: 0),
